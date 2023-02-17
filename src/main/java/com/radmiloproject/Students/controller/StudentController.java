1dab543b467c75c7ad00badbd914841a15aee4ff
RadmiloM <markovicradmilo35@gmail.com>
2023-02-17 12:33:53 +0100

    get mapping method created.

--- src/main/java/com/radmiloproject/Students/controller/StudentController.java
+++ src/main/java/com/radmiloproject/Students/controller/StudentController.java
@@ -6,10 +6,9 @@ import com.radmiloproject.Students.modal.StudentModal;
 import com.radmiloproject.Students.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -24,5 +23,12 @@ public class StudentController {
         Student student = studentMapping.mapToEntity(studentModal);
         studentService.createStudent(student);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/getStudent/{id}")
+    public ResponseEntity<StudentModal> getStudent(@PathVariable("id") UUID uuid){
+        Student student = studentService.getStudent(uuid);
+        StudentModal studentModal = studentMapping.mapToDTO(student);
+        return ResponseEntity.ok(studentModal);
     }
 }
