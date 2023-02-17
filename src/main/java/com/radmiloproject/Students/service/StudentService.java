ed14b58235db71c88a1d1a302b8fcb8855ce6348
RadmiloM <markovicradmilo35@gmail.com>
2023-02-17 12:42:32 +0100

    find all students method created.

--- src/main/java/com/radmiloproject/Students/service/StudentService.java
+++ src/main/java/com/radmiloproject/Students/service/StudentService.java
@@ -5,6 +5,9 @@ import com.radmiloproject.Students.repository.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class StudentService {
@@ -12,6 +15,14 @@ public class StudentService {
 
     public void createStudent(Student student) {
         studentRepository.save(student);
+    }
+
+    public Student getStudent(UUID uuid){
+        return studentRepository.findById(uuid).get();
+    }
+
+    public List<Student> fetchAllStudents(){
+        return studentRepository.findAll();
     }
 
 
