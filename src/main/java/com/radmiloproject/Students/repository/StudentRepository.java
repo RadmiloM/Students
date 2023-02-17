fa5a96cb511c80cdb36c253a9955fcacfbe2b2fe
RadmiloM <markovicradmilo35@gmail.com>
2023-02-17 11:21:02 +0100

    Student repository created.

---
+++ src/main/java/com/radmiloproject/Students/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.radmiloproject.Students.repository;
+
+import com.radmiloproject.Students.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}
