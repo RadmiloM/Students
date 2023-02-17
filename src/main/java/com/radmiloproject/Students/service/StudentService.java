9c27ebdcfba6412c8c096e3ab6a1bf60ddfd2444
RadmiloM <markovicradmilo35@gmail.com>
2023-02-17 13:06:33 +0100

    update student method in service created.

--- src/main/java/com/radmiloproject/Students/service/StudentService.java
+++ src/main/java/com/radmiloproject/Students/service/StudentService.java
@@ -3,9 +3,11 @@ package com.radmiloproject.Students.service;
 import com.radmiloproject.Students.entity.Student;
 import com.radmiloproject.Students.repository.StudentRepository;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,12 +19,30 @@ public class StudentService {
         studentRepository.save(student);
     }
 
-    public Student getStudent(UUID uuid){
+    public Student getStudent(UUID uuid) {
         return studentRepository.findById(uuid).get();
     }
 
-    public List<Student> fetchAllStudents(){
+    public List<Student> fetchAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public void updateStudent(Student student, UUID uuid) {
+        Student studentDB = studentRepository.findById(uuid).get();
+        if (student.getFirstName() != null && !"".equals(student.getFirstName())) {
+            studentDB.setFirstName(student.getFirstName());
+        }
+        if(student.getLastName() !=null && !"".equals(student.getLastName())){
+            studentDB.setLastName(student.getLastName());
+        }
+        if(student.getIndexNumber() != null && !"".equals(student.getIndexNumber())){
+            studentDB.setIndexNumber(student.getIndexNumber());
+        }
+        if(student.getPhoneNumber() != null && !"".equals(student.getPhoneNumber())){
+            studentDB.setPhoneNumber(student.getPhoneNumber());
+        }
+        studentRepository.save(studentDB);
+
     }
 
 
